b124294ffc929f531bb68aaae7ed806600099077
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-10 15:33:32 +0200

    handleChange

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,11 +2,30 @@ import React, { useState } from "react";
 
 function Form(props) {
   const [name, setName] = useState("");
-
+  
   function handleSubmit(e) {
     e.preventDefault();
     props.addTask("Say hello!");
+
+
+  }// near the top of the `Form` component
+function handleChange(e) {
+    setName(e.target.value);
   }
+  
+  // Down in the return statement
+  <input
+    type="text"
+    id="new-todo-input"
+    className="input input__lg"
+    name="text"
+    autoComplete="off"
+    value={name}
+    onChange={handleChange}
+  />
+  
+
+
 
   return (
     <form onSubmit={handleSubmit}>

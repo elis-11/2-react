fe626f0156db580ff9897d84fe7a86b562c92bb9
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-10 19:40:16 +0200

    Defining  filters

--- src/App.js
+++ src/App.js
@@ -6,8 +6,18 @@ import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
 import Todo from "./components/Todo";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All')
 
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map((task) => {

3ba5bd0a16bdf7f3e36bfcba3a38c8cbc6310ccd
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-10 21:29:22 +0200

    useEffect

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,28 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function Todo(props) {
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState("");
 
-  const editFileRef = useRef(null);
+  const editFieldRef = useRef(null);
   const editButtonRef = useRef(null);
+
+  const wasEditing = usePrevious(isEditing);
+
+  function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+      ref.current = value;
+    });
+    return ref.current;
+  }
+  
 
   function handleChange(e) {
     setNewName(e.target.value);
   }
 
+  console.log("main render");
   function handleSubmit(e) {
     e.preventDefault();
     props.editTask(props.id, newName);
@@ -30,7 +42,7 @@ function Todo(props) {
           type="text"
           value={newName}
           onChange={handleChange}
-          ref={editFileRef}
+          ref={editFieldRef}
         />
       </div>
       <div className="btn-group">
@@ -83,6 +95,16 @@ function Todo(props) {
       </div>
     </div>
   );
+
+  useEffect(() => {
+    if (!wasEditing && isEditing) {
+      editFieldRef.current.focus();
+    }
+    if (wasEditing && !isEditing) {
+      editButtonRef.current.focus();
+    }
+  }, [wasEditing, isEditing]);
+  
 
   return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 }

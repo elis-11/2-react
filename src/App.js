526e8e6533ed99203f6bb31cede3fdcf605283c4
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-10 14:29:49 +0200

    const setName

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.scss";
+import FilterButton from "./components/FilterButton";
+import Form from "./components/Form";
 import Todo from "./components/Todo";
 
 /* eslint-disable jsx-a11y/no-redundant-roles */
 function App(props) {
+
+  function addTask(name){
+    alert(name)
+  }
 
   const taskList = props.tasks.map((task) =>
    <Todo 
@@ -14,41 +20,15 @@ function App(props) {
 
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <h1>TodoList</h1>
+      <Form addTask={addTask} />
+
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+      <FilterButton />
+      <FilterButton />
+      <FilterButton />
       </div>
+      
       <h2 id="list-heading">3 tasks remaining</h2>
       <ul
         role="list"

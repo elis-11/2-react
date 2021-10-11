a6ee18785856623aec1db49d03fcad58f0500b6f
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-11 21:22:51 +0200

    fundament-curs

--- src/index.js
+++ src/index.js
@@ -2,15 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const DATA=[
-  {id: "todo-0", name: "Wake up", completed:true},
-  {id: "todo-1", name: "Eat", completed:false},
-  {id: "todo-2", name: "Sleep", completed:false},
-]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App tasks={DATA} />
-  </React.StrictMode>,
+<App />,
   document.getElementById("root")
 );

4883d4bee633ffa5d4be010fbe16de5fd3890627
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-11 16:54:05 +0200

    started FORM-REACT

--- src/App.js
+++ src/App.js
@@ -1,60 +1,11 @@
-/* eslint-disable jsx-a11y/no-redundant-roles */
 import React from 'react'
-import './App.scss'
-import Todo from './components/Todo';
 
-function App(props) {
+function App() {
   return (
-    <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="completed={}tex id='todo-0't"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
-      <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-      </div>
-      <h2 id="list-heading">
-        3 tasks remaining
-      </h2>
-      <ul
-        role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading"
-      >
-        <Todo name='Wake up' completed={true} id='todo-0'/>
-        <Todo name='Eat' completed={false} id='todo-1'/>
-        <Todo name='Sleep' completed={false} id='todo-1'/>
-      </ul>
+    <div>
+      ggg
     </div>
-  );
+  )
 }
 
 export default App

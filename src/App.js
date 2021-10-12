6d38852982702089c0aaa476e865a3975eafb140
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-12 11:28:42 +0200

    input

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 
 function App() {
 const [likes, setLikes] = useState(6)
+const [value, setValue] = useState('Text in Input')
 
 function increment(){
   setLikes(likes-1)
@@ -14,6 +15,12 @@ function decrement(){
   return (
     <div>
       <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input 
+      type="text"
+      value={value}
+      onChange={event => setValue(event.target.value)}
+      />
       <button onClick={increment}>increment</button>
       <button onClick={decrement}>decrement</button>
     </div>

d5fcdda3619839334bee9cb4a8320d44d13193b4
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-12 13:54:23 +0200

    created scss/App.scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
-import Counter from "./components/Counter";
+// import Counter from "./components/Counter";
 
 function App() {
-  const [value, setValue] = useState('Text')
+  const [value, setValue] = useState("Text");
 
   return (
     <div className="App">
-<Counter />
+      <div className="post">
+        <div className="post__content">
+          <strong>1. JavaScript</strong>
+          <div>
+              JavaScript is important for performance!
+          </div>
+        </div>
+        <div className="post__btns">
+<button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 }

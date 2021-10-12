3e6781e5de3bf51f1a7cf6bd2a4fa0988cb463a7
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-12 11:22:53 +0200

    one more time onClick

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
+
+
 
 function App() {
-  const [likes, setLikes] = useState(9);
+const [likes, setLikes] = useState(6)
 
-  function increment() {
-    setLikes(likes + 1);
-  }
-  function decrement() {
-    setLikes(likes - 1);
-  }
+function increment(){
+  setLikes(likes-1)
+}
+function decrement(){
+  setLikes(likes-1)
+}
   return (
     <div>
       <h1>{likes}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

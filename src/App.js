6eaf9bb4606289dcc93e7efb1ab96565bcb010a3
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-13 10:59:25 +0200

    PostForm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useRef, useState } from "react";
+import PostForm from "./components/PostForm";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
@@ -12,35 +13,15 @@ function App() {
     { id: 1, title: "Javascript", body: "Description" },
     { id: 2, title: "Javascript-2", body: "Description" },
     { id: 3, title: "Javascript-3", body: "Description" },
-  ]);
+  ]); 
 
-  const [post, setPost] = useState({ title: "", body: "" });
-  //  const [body, setBody] = useState('')
-
-  const addNewPost = (e) => {
-    e.preventDefault();
-    setPosts([...posts, {...post, id: Date.now()}]);
-    setPost({ title: "", body: "" });
-  };
+const createPost=(newPost)=>{
+  setPosts([...posts, newPost])
+}
 
   return (
     <div className="App">
-      <form>
-        <MyInput
-          value={post.title}
-          onChange={(e) => setPost({ ...post, title: e.target.value })}
-          type="text"
-          placeholder="Name of Post"
-        />
-
-        <MyInput
-          value={post.body}
-          onChange={(e) => setPost({ ...post, body: e.target.value })}
-          type="text"
-          placeholder="Description"
-        />
-        <MyButton onClick={addNewPost}>Create a Post</MyButton>
-      </form>
+      <PostForm create={createPost} />
       <PostList posts={posts} title="JS Posts" />
     </div>
   );

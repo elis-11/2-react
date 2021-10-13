939a74bdbecaf0193f2b326618a0bea4e29ecec2
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-13 20:41:04 +0200

    HOOK-useFetching

--- src/App.js
+++ src/App.js
@@ -10,33 +10,30 @@ import MyModal from "./components/UI/MyModal/MyModal";
 import MySelect from "./components/UI/select/MySelect";
 import "./scss/App.scss";
 import { usePosts } from "./hooks/usePosts";
+import { useFetching } from "./hooks/usePosts";
 import axios from "axios";
 import PostService from "./API/PostServise";
+import Loader from "./components/UI/Loader/Loader";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostLoading, setIsPostLoading] = useState(false);
+  const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
 
   useEffect(() => {
     fetchPosts();
-  }, []);
+  }, [ ]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
 
-  async function fetchPosts() {
-    setIsPostLoading(true);
-    setTimeout(async () => {
-      const posts = await PostService.getAll();
-      setPosts(posts);
-      setIsPostLoading(false);
-    }, 5000);
-  }
   //get post from children components
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -53,9 +50,21 @@ function App() {
       </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
+      {postError && 
+      <h1>Error ${postError}</h1>
+      }
       {isPostLoading ? (
-        <h1>Loading...</h1>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "100px",
+          }}
+        >
+          <Loader />
+        </div>
       ) : (
+        // <h1>Loading...</h1>
         <PostList
           remove={removePost}
           posts={sortedAndSearchedPosts}

48ece1058900613d6a93ba721ecbb111c71c6580
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-13 16:34:27 +0200

    usePosts

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -9,3 +9,14 @@ export const useSortedPosts = (posts, sort) => {
   }, [sort, posts]);
   return sortedPosts;
 };
+
+export const usePosts = (posts, sort, query) => {
+  const sortedPosts = useSortedPosts(posts, sort);
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter((post) =>
+      post.title.toLowerCase().includes(query.toLowerCase())
+    );
+  }, [query, sortedPosts]);
+  return sortedAndSearchedPosts;
+};

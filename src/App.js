a928577702d430369fd92c53adf404bdf40ea0b2
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-13 10:11:51 +0200

    useState --change string to object

--- src/App.js
+++ src/App.js
@@ -14,36 +14,30 @@ function App() {
     { id: 3, title: "Javascript-3", body: "Description" },
   ]);
 
-   const [title, setTitle] = useState("");
-   const [body, setBody] = useState('')
+  const [post, setPost] = useState({ title: "", body: "" });
+  //  const [body, setBody] = useState('')
 
-  const addNewPost=(e) =>{
+  const addNewPost = (e) => {
     e.preventDefault();
-const newPost ={
-  id: Date.now(),
-  title,
-  body
-}
-setPosts([...posts, newPost])
-setTitle("")
-setBody("")
-}
+    setPosts([...posts, {...post, id: Date.now()}]);
+    setPost({ title: "", body: "" });
+  };
 
   return (
     <div className="App">
       <form>
         <MyInput
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          value={post.title}
+          onChange={(e) => setPost({ ...post, title: e.target.value })}
           type="text"
           placeholder="Name of Post"
         />
 
         <MyInput
-        value={body}
-        onChange={(e) => setBody(e.target.value)}
-        type="text" 
-        placeholder="Description" 
+          value={post.body}
+          onChange={(e) => setPost({ ...post, body: e.target.value })}
+          type="text"
+          placeholder="Description"
         />
         <MyButton onClick={addNewPost}>Create a Post</MyButton>
       </form>

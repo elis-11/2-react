ff708094c20dd6198ce59e867c31d1033806fc89
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-13 16:52:14 +0200

    created a second HOOK

--- src/App.js
+++ src/App.js
@@ -8,29 +8,18 @@ import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MySelect from "./components/UI/select/MySelect";
-// import "./reset.css";
 import "./scss/App.scss";
+import { usePosts } from "./hooks/usePosts";
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "Javascript", body: "Description" },
-    { id: 2, title: "Script-2", body: "De" },
-    { id: 3, title: "JS-3", body: "Script" },
-  ]);
-
-const [filter, setFilter]=useState({sort:'', query:''});
-const [modal, setModal] = useState(false)
-
-  function getSortedPosts() {}
-
-
-  const sortedAndSearchedPosts= useMemo(() => {
-return sortedPosts.filter(post=>post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, sortedPosts]);
+  const [posts, setPosts] = useState([]);
+  const [filter, setFilter] = useState({ sort: "", query: "" });
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
-    setModal(false)
+    setModal(false);
   };
   //get post from children components
   const removePost = (post) => {
@@ -39,18 +28,19 @@ return sortedPosts.filter(post=>post.title.toLowerCase().includes(filter.query.t
 
   return (
     <div className="App">
-      <MyButton style={{ marginTop: '30px' }} onClick={()=>setModal(true)}>
+      <MyButton style={{ marginTop: "30px" }} onClick={() => setModal(true)}>
         create an user
-        </MyButton>
+      </MyButton>
       <MyModal visible={modal} setVisible={setModal}>
-      <PostForm create={createPost} />
-        </MyModal>
+        <PostForm create={createPost} />
+      </MyModal>
       <hr style={{ margin: "15px 0" }} />
-     <PostFilter
-     filter={filter}
-     setFilter={setFilter}
-     />
-        <PostList remove={removePost} posts={sortedAndSearchedPosts} title="JS Posts" />
+      <PostFilter filter={filter} setFilter={setFilter} />
+      <PostList
+        remove={removePost}
+        posts={sortedAndSearchedPosts}
+        title="JS Posts"
+      />
     </div>
   );
 }
--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -20,3 +20,4 @@ export const usePosts = (posts, sort, query) => {
   }, [query, sortedPosts]);
   return sortedAndSearchedPosts;
 };
+

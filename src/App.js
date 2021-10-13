e8ae62b528653192325d8bd8998a35406226149e
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-13 11:43:37 +0200

    DROPDOWN sorting by

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import MySelect from "./components/UI/select/MySelect";
 // import "./reset.css";
 import "./scss/App.scss";
 
@@ -26,6 +27,17 @@ const removePost=(post)=>{
   return (
     <div className="App">
       <PostForm create={createPost} />
+      <hr style={{margin: '15px 0'}} />
+      <div>
+        <MySelect 
+        defaultValue='sort by'
+        options={[
+          {value: 'title', name: 'name'},
+          {value: 'body', name: 'description'},
+        ]}
+        />
+      </div>
+
       {posts.length
       ? <PostList remove={removePost} posts={posts} title="JS Posts" />
       : <h1 style={{textAlign: 'center'}}>

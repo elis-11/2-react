69cb63a178ce3b1a013bf58b78aad03844ad866a
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-14 11:44:13 +0200

    start again

--- src/components/UI/button/MyButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./MyButton.scss";
-
-function MyButton({children, ...props}) {
-  return <button {...props} className="my-btn">{children}</button>;
-}
-
-export default MyButton;

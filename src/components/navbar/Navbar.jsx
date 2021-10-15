8030633da7f125a840a1dc787a3db949423d7a4f
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-15 19:59:13 +0200

    Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,26 +1,35 @@
-import { NavLink } from 'react-router-dom';
-import './Navbar.scss'
+import { NavLink } from "react-router-dom";
+import Icons from "./icons/Icons";
+import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <nav className='nav'>
-      <div className='item'>
-        {/* <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink> */}
-        <NavLink to="/profile">Profile</NavLink>
-
+    <nav className="nav">
+      <div>
+        <img
+          className="logo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcJV8o6WqlDiYhUlLyRcwKxRvj1S36C6a3_g&usqp=CAU"
+          alt=""
+        />
       </div>
-      <div className=''>
-        <NavLink to="/dialogs">Message</NavLink>
+      <div className="link">
+        <div className="item">
+          <NavLink to="/profile">Profile</NavLink>
+        </div>
+        <div className="">
+          <NavLink to="/dialogs">Message</NavLink>
+        </div>
+        <div className="">
+          <NavLink to="/news">News</NavLink>
+        </div>
+        <div className="">
+          <NavLink to="/about">About</NavLink>
+        </div>
+        <div className="">
+          <NavLink to="/settings">Settings</NavLink>
+        </div>
       </div>
-      <div className=''>
-        <NavLink to="/news">News</NavLink>
-      </div>
-      <div className=''>
-        <NavLink to="/about">About</NavLink>
-      </div>
-      <div className=''>
-        <NavLink to="/settings">Settings</NavLink>
-      </div>
+     <Icons />
     </nav>
   );
 };

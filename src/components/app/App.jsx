86d96ec4db30b69334e54d823db8e5c9c4753909
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-15 21:56:33 +0200

    App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,26 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router, Switch} from "react-router-dom";
 import "./App.scss";
 import "../../reset.css";
 import Navbar from "../navbar/Navbar";
-// import Home from "../home/Home";
-// import Works from "../works/Works";
-// import About from "../about/About";
+import Home from "../home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <div className="contentt">
-          <Navbar />
-          {/* <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/works" exact component={Works} />
-          <Route path="/about" exact component={About} />
-        </Switch> */}
-        </div>
-      </Router>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Home />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

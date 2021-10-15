b9cf6e5022eb0ceeacee0fb00695cc8f1af499f6
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-15 19:58:51 +0200

    first extensions

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
+import "../../reset.css";
 import Navbar from "../navbar/Navbar";
 // import Home from "../home/Home";
 // import Works from "../works/Works";
 // import About from "../about/About";
 
-
 function App() {
   return (
     <div className="App">
-    <Router>
-      <div className="contentt">
-        <Navbar />
-        {/* <Switch>
+      <Router>
+        <div className="contentt">
+          <Navbar />
+          {/* <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/works" exact component={Works} />
           <Route path="/about" exact component={About} />
         </Switch> */}
-      </div>
-    </Router>
-
+        </div>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

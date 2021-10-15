09c65dd4060b53906d577a0d09dc18b2320720af
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-15 16:21:09 +0200

    fix file

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -7,8 +7,10 @@ const PostList = ({ posts, title }) => {
     <div>
       <h1 style={{ textAlign: "center" }}>{title}</h1>
       {posts.map((post) => (
+
         <PostItem post={post} key={post.id} />
       ))}
+      
     </div>
   );
 };

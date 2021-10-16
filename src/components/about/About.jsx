010292e72a412f2078b3d181c74ee2e17e11d579
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-16 18:53:36 +0200

    import about.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,7 +4,15 @@ import './About.scss'
 const About = () => {
     return (
 <div className="About content">
-    ABOUT
+<h2>About</h2>
+			<p>Page 2</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo
+				possimus assumenda suscipit ducimus eius! Est ab consequuntur aspernatur
+				inventore nulla! Quae reprehenderit eligendi beatae nemo maiores et
+				officiis, quasi excepturi placeat natus commodi, doloremque distinctio
+				distinctio!
+			</p>
     </div>
 
         )

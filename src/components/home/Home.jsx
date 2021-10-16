2832ae1e200b4d799b59237e13c033bc79e1e928
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-16 18:52:27 +0200

    create Home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import "./Home.scss";
 
 const Home = () => {
-  return <div className="Home content">
-      HOME
-      </div>;
+  return (
+    <div className="Home content">
+      <h2>Home</h2>
+      <p>Page 1</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo
+        possimus assumenda suscipit ducimus eius! Est ab consequuntur aspernatur
+        inventore nulla! Quae reprehenderit eligendi beatae nemo maiores et
+        officiis, quasi excepturi placeat natus commodi, doloremque distinctio
+        distinctio!
+      </p>
+    </div>
+  );
 };
 
 export default Home;

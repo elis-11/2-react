a8513ae55b7723de89a558a0155df14997787f75
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-16 18:54:05 +0200

    created Page.jsx and scss

---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Projects.scss";
+
+const Projects = () => {
+  return (
+    <div className="Projects content">
+      <h2>Projects</h2>
+      <p>Page 3</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo
+        possimus assumenda suscipit ducimus eius! Est ab consequuntur aspernatur
+        inventore nulla! Quae reprehenderit eligendi beatae nemo maiores et
+        officiis, quasi excepturi placeat natus commodi, doloremque distinctio
+        distinctio!
+      </p>
+    </div>
+  );
+};
+export default Projects;

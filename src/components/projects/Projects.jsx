f6165821b3596e092c4482f12ed3142cd3673ddd
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-17 19:35:52 +0200

    styled Count.scss

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import Count from "./Count";
+import Count from "./count/Count";
 import "./Projects.scss";
 
 const Projects = () => {
   return (
-    <div className="Projects content">
-      <h2>Projects</h2>
-      <p>Page 3</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo
-        possimus assumenda suscipit ducimus eius! Est ab consequuntur aspernatur
-        inventore nulla! Quae reprehenderit eligendi beatae nemo maiores et
-        officiis, quasi excepturi placeat natus commodi, doloremque distinctio
-        distinctio!
-      </p>
-      <Count />
+    <div className="Projects">
+        {/* <Count /> */}
+      <div className="post">
+        <div className="post-content">
+          <strong>My Project</strong>
+          <div>API</div>
+        </div>
+
+        <div className="btn">
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/projects/count/Count.jsx
+++ src/components/projects/count/Count.jsx
@@ -1,26 +1,26 @@
 import React, { useState } from "react";
 
 const Count = () => {
-  const [likes, setLikes] = useState(5);
-  const [value, setValue] = useState('Text')
+  const [count, setCount] = useState(5);
+  // const [value, setValue] = useState('Text')
 
   function increment() {
-    setLikes(likes + 1);
+    setCount(count + 1);
   }
   function decrement() {
-    setLikes(likes - 1);
+    setCount(count - 1);
   }
 
   return (
     <div className='Count'>
-      <h2>{likes}</h2>
-      <h2>{value}</h2>
+      <h2>{count}</h2>
+      {/* <h2>{value}</h2>
       <input 
       type="text"
       value={value}
     //   onChange={event => event.target.value}
-      onChange={event => setLikes(event.target.value)}
-      />
+      onChange={event => setCount(event.target.value)}
+      /> */}
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>

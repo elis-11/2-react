46dc8d89e6cc81523197f803d2c3ed5d257b406f
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-17 21:06:56 +0200

    posts version-2

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,20 +1,22 @@
-import React from "react";
-import Count from "./count/Count";
+import React, { useState } from "react";
 import "./Projects.scss";
+// import PostItem from "./PostItem";
+import PostList from "./PostList";
 
 const Projects = () => {
+  const [posts, setPosts] = useState([
+    { id: "1", title: "JS", body: "Description" },
+    { id: "2", title: "JS", body: "Description" },
+  ]);
+  const [posts2, setPosts2] = useState([
+    { id: "1", title: "Python", body: "Description" },
+    { id: "2", title: "Python", body: "Description" },
+  ]);
+
   return (
     <div className="Projects">
-      {/* <Count /> */}
-      <div className="post">
-        <div className="post-content">
-          <strong>1. Project</strong>
-          <div>API</div>
-        </div>
-        <div className="btn">
-          <button>Delete</button>
-        </div>
-      </div>
+      <PostList posts={posts} title="All Posts 1" />
+      <PostList posts={posts2} title="All Posts 2" />
     </div>
   );
 };

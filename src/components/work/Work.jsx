952c9708b7df4211163c1df6f9f8e27663a2b443
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-22 19:09:34 +0200

    added Work.jsx & scss

---
+++ src/components/work/Work.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Work.scss";
+
+const Work = () => {
+  return (
+    <div className="Work content">
+      <h2>Work</h2>
+      <p>Page </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo
+        possimus assumenda suscipit ducimus eius! Est ab consequuntur aspernatur
+        inventore nulla! Quae reprehenderit eligendi beatae nemo maiores et
+        officiis, quasi excepturi placeat natus commodi, doloremque distinctio
+        distinctio!
+      </p>
+    </div>
+  );
+};
+
+export default Work;

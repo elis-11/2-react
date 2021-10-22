b3564b9e9143bb74bb1e751517c3a0220792973c
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-22 19:30:20 +0200

    modified News.jsx

---
+++ src/components/news/News.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./News.scss";
+
+const News = () => {
+  return (
+    <div className="News content">
+      <h2>News</h2>
+      <p>Page </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo
+        possimus assumenda suscipit ducimus eius! Est ab consequuntur aspernatur
+        inventore nulla! Quae reprehenderit eligendi beatae nemo maiores et
+        officiis, quasi excepturi placeat natus commodi, doloremque distinctio
+        distinctio!
+      </p>
+    </div>
+  );
+};
+
+export default News;

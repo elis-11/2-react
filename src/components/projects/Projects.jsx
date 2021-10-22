8c3aa1fedd727b4c57feadad1b72e0cce6f7e334
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-23 00:37:44 +0200

    PostForm

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,48 +2,23 @@ import React, { useState } from "react";
 import "./Projects.scss";
 // import PostItem from "./PostItem";
 import PostList from "./PostList";
-import MyButton from "../UI/button/MyButton";
-import MyInput from "../UI/input/MyInput";
+// import MyButton from "../UI/button/MyButton";
+// import MyInput from "../UI/input/MyInput";
+import PostForm from "./PostForm";
 
-const Projects = () => {
-  // eslint-disable-next-line no-unused-vars
+function Projects() {
   const [posts, setPosts] = useState([
     { id: "1", title: "JS", body: "Description" },
     { id: "2", title: "JS", body: "Description" },
   ]);
 
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-
-  const addNewPost = (e) => {
-    e.preventDefault();
-    const newPost={
-      id: Date.now(),
-      title,
-      body
-    }
+  const createPost = (newPost) => {
     setPosts([...posts, newPost]);
-    setTitle('')
-    setBody('')
-  };
+  }
 
   return (
     <div className="Projects">
-      <form>
-        <MyInput
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          type="text"
-          placeholder="Name"
-        />
-        <MyInput
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-          type="text"
-          placeholder="Description"
-        />
-        <MyButton onClick={addNewPost}>create a post</MyButton>
-      </form>
+      <PostForm create={createPost}/>
       <PostList posts={posts} title="All Posts" />
     </div>
   );

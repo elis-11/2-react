14455380f364b355025f2b4161b0633c9ca63782
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-07 14:17:58 +0100

    styled NEWS

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -5,14 +5,7 @@ const News = () => {
   return (
     <div className="News content">
       <h2>News</h2>
-      <p>Page </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo
-        possimus assumenda suscipit ducimus eius! Est ab consequuntur aspernatur
-        inventore nulla! Quae reprehenderit eligendi beatae nemo maiores et
-        officiis, quasi excepturi placeat natus commodi, doloremque distinctio
-        distinctio!
-      </p>
+      <h2>UseState with Objects</h2>
     </div>
   );
 };

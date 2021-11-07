249be5c59d4afd823a0bd09b3e8f8673f60192d2
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-07 15:00:55 +0100

    UseState

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import {useState} from "react";
 import "./News.scss";
 
 const News = () => {
+const [suppliers, setSuppliers] = useState([
+  {_id: 's1', company:'SU GmbH', contact: 'Albert', phone: '+496328642'},
+  {_id: 's2', company:'SE GmbH', contact: 'Bob', phone: '+496328642'},
+])
+const jsxSuppliers =suppliers.map((supplier) =>(
+  <div key={supplier._id}>
+<form>
+  <div>
+  <label>Company: </label><input type="text" name="company" value={ supplier.company } />
+  <label>Contact: </label><input type="text" name="contact" value={ supplier.contact } />
+  </div>
+</form>
+  </div>
+))
   return (
     <div className="News content">
+      <header className='News-header'>
       <h2>News</h2>
       <h2>UseState with Objects</h2>
+      {jsxSuppliers}
+      </header>
     </div>
   );
 };

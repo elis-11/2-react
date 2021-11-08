b150cf8b2e6906f79d0271cc25753ac8d86b553b
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-08 21:16:01 +0100

    added Calendar folder

---
+++ src/components/calendar/Calendar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Calendar.scss";
+
+const Calendar = () => {
+  return (
+    <div className="Calendar content">
+      <h2>Calendar</h2>
+      <p>Page </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo
+        possimus assumenda suscipit ducimus eius! Est ab consequuntur aspernatur
+        inventore nulla! Quae reprehenderit eligendi beatae nemo maiores et
+        officiis, quasi excepturi placeat natus commodi, doloremque distinctio
+        distinctio!
+      </p>
+    </div>
+  );
+};
+
+export default Calendar;

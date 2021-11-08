c094f1da606759f63c030b0f1f775440e18b8be2
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-08 21:16:40 +0100

    createdettings.jsx S

---
+++ src/components/settings/Settings.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Settings.scss";
+
+const Settings = () => {
+  return (
+    <div className="Settings content">
+      <h2>Settings</h2>
+      <p>Page </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo
+        possimus assumenda suscipit ducimus eius! Est ab consequuntur aspernatur
+        inventore nulla! Quae reprehenderit eligendi beatae nemo maiores et
+        officiis, quasi excepturi placeat natus commodi, doloremque distinctio
+        distinctio!
+      </p>
+    </div>
+  );
+};
+
+export default Settings;

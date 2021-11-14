e51900569a4725ce8817fe9ca82dc87460292b45
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-14 16:30:52 +0100

    fix file

--- src/components/hooks/useSt/UseSt.jsx
+++ src/components/hooks/useSt/UseSt.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './UseSt.scss'
 
 const UseSt = () => {
 
+const [counter, setCounter] = useState(0)
 
   return (
     <div className='UseSt header'>
+      <h2>Counter: {counter}</h2> 
+      <button className='Add'>Add</button>
+      <button className='Delete'>Delete</button>
       
     </div>
   )

9a065d2f47a6b030e17c7b26d3b86a1314e62a71
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-14 20:51:47 +0100

    UseEffect-vers-1

--- src/components/hooks/useEff/UseEff.jsx
+++ src/components/hooks/useEff/UseEff.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./UseEff.scss";
 
 const UseEff = () => {
+  const [type, setType] = useState("users");
+  const [data, setData] = useState([]);
+  // useEffect(() =>{
+  //   console.log('render');
+  // })
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+      .then((response) => response.json())
+      .then((json) => setData(json));
+    // console.log('Type change', type);
+  }, [type]);
+
   return (
-    <div>
+    <div className="UseEff">
       <h2>UseEffect</h2>
+      <h2>Resurses: {type}</h2>
+
+      <button onClick={() => setType("Users")}>Users</button>
+      <button onClick={() => setType("Todos")}>Todos</button>
+      <button onClick={() => setType("Posts")}>Posts</button>
+
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 };

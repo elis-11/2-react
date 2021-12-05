b59131b1a4d659a486e3fa698fd0b80a534e925c
elis-11 <eliza.arzanukaeva@gmail.com>
2021-12-05 12:46:21 +0100

    FORM works

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import { useState, useEffect } from 'react';
 import './Form.scss';
@@ -12,14 +13,41 @@ export const Form = () => {
 	const [ phone, setPhone ] = useState('');
 	const [ phoneIsValid, setPhoneIsValid ] = useState(false);
 
+	const [ password1, setPassword1 ] = useState([]);
+	const [ password1IsValid, setPassword1IsValid ] = useState(false);
+	const [ passwordsInputType, setPasswordsInputType ] = useState('psaaword');
+
+	const [ password2, setPassword2 ] = useState([]);
+	const [ password2IsValid, setPassword2IsValid ] = useState(false);
+
 	const [ payload, setPayload ] = useState({});
 	const [ formIsValid, setFormIsValid ] = useState(false);
 
+	const clearPayload = () => {
+		if (Object.keys(payload).length !== 0) {
+			setPayload((prev) => ({}));
+		}
+	};
+
 	useEffect(
 		() => {
-			setFormIsValid(nameIsValid && emailIsValid && phoneIsValid);
+			clearPayload();
 		},
-		[ nameIsValid, emailIsValid, phoneIsValid ]
+		[ name, email, phone, password1, password2 ]
+	);
+
+	useEffect(
+		() => {
+			setFormIsValid(
+				nameIsValid &&
+					emailIsValid &&
+					phoneIsValid &&
+					password1IsValid &&
+					password2IsValid &&
+					password1 === password2
+			);
+		},
+		[ nameIsValid, emailIsValid, phoneIsValid, password1IsValid, password2IsValid ]
 	);
 
 	const handleName = (e) => {
@@ -50,18 +78,46 @@ export const Form = () => {
 		setPhone(_phone);
 	};
 
-	const clearPayload = () => {
-		setPayload((prev) => ({}));
+	const handlePassword1 = (e) => {
+		let _password1 = e.target.value;
+		_password1.length >= 5 ? setPassword1IsValid(true) : setPassword1IsValid(false);
+		setPassword1(_password1);
 	};
-	// clearPayload();
+
+	const handlePassword2 = (e) => {
+		let _password2 = e.target.value;
+		_password2.length >= 5 ? setPassword2IsValid(true) : setPassword2IsValid(false);
+		setPassword2(_password2);
+	};
+
+	const handleShowPasswordButton = (e) => {
+		setPasswordsInputType(passwordsInputType === 'password' ? 'text' : 'password');
+	};
 
 	const handleButton = (e) => {
 		e.preventDefault();
 		setPayload((prev) => ({
 			...prev,
 			name,
+			email,
+			password1,
+			password2,
 			phone
 		}));
+
+		const requestOptions = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				name,
+				email,
+				phone,
+				password1,
+				password2
+			})
+		}(async () => {
+			await fetch(`http://localhost:3033/create`, requestOptions);
+		})();
 	};
 
 	return (
@@ -75,7 +131,7 @@ export const Form = () => {
 						<label htmlFor="name">Name</label>
 						<input type="text" id="name" value={name} onChange={handleName} />
 					</div>
-					<div className={`note ${nameIsValid ? 'valid' : 'invalid'}`}>required, 2 - 20 characters</div>
+					<div className={`note ${nameIsValid ? 'valid' : 'invalid'}`}>required: 2 - 20 characters</div>
 
 					{/* EMAIL */}
 					<div className={`row + ${emailIsValid ? 'valid' : 'invalid'}`}>
@@ -83,7 +139,7 @@ export const Form = () => {
 						<input type="email" id="name" value={email} onChange={handleEmail} />
 					</div>
 					<div className={`note ${emailIsValid ? 'valid' : 'invalid'}`}>
-						<p>User@mail.com</p>
+						<p>user@mail.com</p>
 					</div>
 
 					{/* PHONE */}
@@ -92,6 +148,31 @@ export const Form = () => {
 						<input type="text" id="phone" value={phone} onChange={handlePhone} />
 					</div>
 					<div className={`note ${phoneIsValid ? 'valid' : 'invalid'}`}>e.g. 5553332222</div>
+
+					{/* PASSWORD-1 */}
+					<div className={`row ${password1IsValid ? 'valid' : 'invalid'}`}>
+						<label htmlFor="password">Password</label>
+						<input type={passwordsInputType} id="name" value={password1} onChange={handlePassword1} />
+					</div>
+					<div className={`note ${password1IsValid ? 'valid' : 'invalid'}`}>
+						<p>4 characters</p>
+					</div>
+
+					{/* PASSWORD-BUTTON */}
+					<div className="buttonRow">
+						<button type="button" onClick={handleShowPasswordButton}>
+							Show Password
+						</button>
+					</div>
+
+					{/* PASSWORD-2 */}
+					<div className={`row ${password2IsValid ? 'valid' : 'invalid'}`}>
+						<label htmlFor="password">Password</label>
+						<input type={passwordsInputType} id="name" value={password2} onChange={handlePassword2} />
+					</div>
+					<div className={`note ${password2IsValid ? 'valid' : 'invalid'}`}>
+						<p>repeat your password</p>
+					</div>
 
 					{/* BUTTON */}
 					<div className="buttonRow">

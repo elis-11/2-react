b473c9b454961dbb360f83c2084bf2570e50afd0
elis-11 <eliza.arzanukaeva@gmail.com>
2022-02-19 22:41:16 +0100

    UseRef- focus- Add

--- src/components/todos/AddItem.js
+++ src/components/todos/AddItem.js
@@ -1,13 +1,18 @@
+import { useRef } from "react";
 import { FaPlus } from "react-icons/fa";
 import "./Todos.scss";
 
 export const AddItem = ({newItem, setNewItem, handleSubmit}) => {
+const inputRef= useRef()
+
+
   return (
     <form className="Add" onSubmit={handleSubmit}>
       <div className="container">
         <label htmlFor="addItem">Add Item</label>
         <input
           autoFocus
+          ref={inputRef} 
           id="addItem"
           type="text"
           placeholder="Add Item"
@@ -15,7 +20,11 @@ export const AddItem = ({newItem, setNewItem, handleSubmit}) => {
           value={newItem}
           onChange={(e) => setNewItem(e.target.value)}
         />
-        <button type="submit" aria-label="Add Item">
+        <button 
+        type="submit" 
+        aria-label="Add Item"
+        onClick={()=> inputRef.current.focus()}
+        >
           <FaPlus />
         </button>
       </div>
